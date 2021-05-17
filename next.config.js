e290b331802afb5e5fb2f70df89b431d19d051c4
Aditya Loshali <adityaloshali1@gmail.com>
2021-05-17 08:32:13 -0700

    save up minor fixes

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 // next.config.js
 module.exports = {
   target: "serverless",
-  domains: ['cdn.shopify.com', 'localhost', 'backend-apphub', 'https://backend-apphub.herokuapp.com'],
   distDir: 'out',
   poweredByHeader: false,
+  images: {
+    domains: ['cdn.shopify.com', 'localhost', 'backend-apphub', 'https://backend-apphub.herokuapp.com'],
+  }
   // images: {
   //   loader: "cloudinary",
   //   path: "https://res.cloudinary.com/apphub-optimizer/image/upload/"
