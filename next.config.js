765ec4af41b3090c85acb257f784a8b998b4effd
Aditya Loshali <adityaloshali1@gmail.com>
2021-05-17 08:03:34 -0700

    next config update

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ module.exports = {
   domains: ['cdn.shopify.com', 'localhost', 'backend-apphub', 'https://backend-apphub.herokuapp.com'],
   distDir: 'out',
   poweredByHeader: false,
-  images: {
-    loader: "cloudinary",
-    path: "https://res.cloudinary.com/apphub-optimizer/image/upload/"
-  }
+  // images: {
+  //   loader: "cloudinary",
+  //   path: "https://res.cloudinary.com/apphub-optimizer/image/upload/"
+  // }
 }
